7a42e065d71ff06eb8476a3b7fde532d248aa0cd
rafno <rafnar@gangverk.is>
2024-02-21 15:10:33 +0000

    Swap to working example

--- dockerfile
+++ dockerfile
@@ -1,18 +1,17 @@
 FROM python:3.10-slim
 
-RUN mkdir -p /opt/dagster/dagster_home /opt/dagster/app
+RUN mkdir -p /opt/dagster/dagster_home
 
-# Copy requirements.txt and install dependencies
-COPY pyproject.toml setup.py setup.cfg /opt/dagster/dagster_home/
+ENV DAGSTER_HOME=/opt/dagster/dagster_home/
+
+COPY pyproject.toml setup.cfg setup.py /opt/dagster/dagster_home/
+
+COPY medicine /opt/dagster/dagster_home/medicine
 
 WORKDIR /opt/dagster/dagster_home
 
 RUN pip install .
 
-ENV DAGSTER_HOME=/opt/dagster/dagster_home/
-
 EXPOSE 3000
 
-# Specify the entrypoint command to start the Dagster web server
-#ENTRYPOINT ["dagster-webserver", "-w", "workspace.yaml", "-h", "0.0.0.0", "-p", "3000"]
 ENTRYPOINT ["dagster", "dev", "-h", "0.0.0.0", "-p", "3000"]
--- setup.py
+++ setup.py
@@ -1,25 +1,8 @@
-import glob
-
 from setuptools import find_packages, setup
 
 setup(
-    name="testing_dagster",
-    packages=find_packages(),
-    install_requires=[
-        "dagster",
-        "dagster-cloud",
-        "boto3",
-        "dagster-dbt",
-        "pandas",
-        "numpy",
-        "scipy",
-        "dbt-core",
-        "dbt-duckdb",
-        "dagster-duckdb",
-        "dagster-webserver",
-        "dagster-duckdb-pandas",
-        # packaging v22 has build compatibility issues with dbt as of 2022-12-07
-        "packaging<22.0",
-    ],
+    name="medicine",
+    packages=find_packages(where=".", exclude=["medicine_tests"]),
+    install_requires=["dagster", "dagster-cloud", "dagster-webserver"],
     extras_require={"dev": ["dagster-webserver", "pytest"]},
 )
