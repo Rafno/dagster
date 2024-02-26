26b42936979e294fa77992684350dc9d6399677b
Rafnar <rafnar1993@gmail.com>
2024-02-26 21:00:10 +0000

    Moved utils to asset. Includes APi call

--- hearthstone/assets/fetchcards/fetchcards.py
+++ hearthstone/assets/fetchcards/fetchcards.py
@@ -2,32 +2,34 @@ from io import BytesIO
 
 import pandas as pd
 import requests
-
+from . import hearthstone_api
 from dagster import AssetExecutionContext, AssetMaterialization, MetadataValue, asset
 
 
 @asset
-def read_excel_file(context):
-    file_url = "https://www.lyfjastofnun.is/wp-content/uploads/2024/02/lyf-med-markadsleyfi-0224.xlsx"
-    response = requests.get(file_url)
-    df = pd.read_excel(BytesIO(response.content))
-    context.log.info(f"Read {len(df)} rows from Excel file.")
+def get_hearthstone_cards(context):
+    cards = hearthstone_api.get_all_hearthstone_cards(context)
+    context.log.info(f"First {cards[:100]} lines")
 
-    # Convert DataFrame to Markdown
-    markdown_str = df.head(5).to_markdown()  # Convert first 5 rows to Markdown
+# @asset
+# def get_hearthstone_cards(context):
+#     file_url = "https://www.lyfjastofnun.is/wp-content/uploads/2024/02/lyf-med-markadsleyfi-0224.xlsx"
+#     response = requests.get(file_url)
+#     df = pd.read_excel(BytesIO(response.content))
+#     context.log.info(f"Read {len(df)} rows from Excel file.")
 
-    # Log metadata and materialize the asset
-    metadata_dict = {
-        # "column_names": list(df.columns),
-        "row_count": len(df),
-        "preview": MetadataValue.md(markdown_str),
-    }
-    context.add_output_metadata(metadata=metadata_dict)
-    context.log.info("Exported DataFrame to Markdown format")
+#     # Convert DataFrame to Markdown
+#     markdown_str = df.head(5).to_markdown()  # Convert first 5 rows to Markdown
 
-    return df
+#     # Log metadata and materialize the asset
+#     metadata_dict = {
+#         # "column_names": list(df.columns),
+#         "row_count": len(df),
+#         "preview": MetadataValue.md(markdown_str),
+#     }
+#     context.add_output_metadata(metadata=metadata_dict)
+#     context.log.info("Exported DataFrame to Markdown format")
+
+#     return df
 
 
-@asset
-def read_my(context):
-    return None
---
+++ hearthstone/assets/fetchcards/hearthstone_api.py
@@ -0,0 +1,49 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+from dagster import AssetExecutionContext, AssetMaterialization, MetadataValue
+
+
+
+def __get_access_token():
+    load_dotenv()
+
+    # Access environment variables
+    BATTLENET_CLIENT_ID = os.getenv("BATTLENET_CLIENT_ID")
+    BATTLENET_CLIENT_SECRET = os.getenv("BATTLENET_CLIENT_SECRET")
+    return BATTLENET_CLIENT_ID, BATTLENET_CLIENT_SECRET
+
+
+def generate_bearer_token(context, client_id, client_secret) -> str:
+    url = "https://us.battle.net/oauth/token"
+    payload = {
+        'grant_type': 'client_credentials'
+    }
+    response = requests.post(url, auth=(client_id, client_secret), data=payload)
+    
+    if response.status_code == 200:
+        return response.json()['access_token']
+    else:
+        context.log.error(response.text)
+        return None
+
+
+def get_all_hearthstone_cards(context) -> str:
+    reqUrl = "https://eu.api.blizzard.com/hearthstone/cards/"
+    client_id, client_secret = __get_access_token()
+    token = generate_bearer_token(context, client_id, client_secret)
+
+    headersList = {
+    "Accept": "*/*",
+    "User-Agent": "application/json",
+    "Authorization": token
+    }
+
+    payload = ""
+
+    response = requests.request("GET", reqUrl, data=payload,  headers=headersList)
+    context.log.info(f"status code was {response.status_code}")
+    return response.text
+
+
